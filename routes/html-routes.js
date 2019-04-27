5fb621f6328cca175b0f176aefdcb605058057e2
desham25 <runwiththebull84@gmail.com>
2019-04-27 15:01:55 -0400

    Post req update

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
   // HTML GET Requests
 
   app.get("", function(req, res) {
-    res.sendFile(path.join(__dirname, ""));
+       res.sendFile(path.join(__dirname, "/../public/survey.html));
   });
 
   // If no matching route is found default to home
