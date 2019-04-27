a7af1bd4f4f98f35c7a7fafa8525d4c8fa368c07
MeronLangsner <46253914+MeronLangsner@users.noreply.github.com>
2019-04-27 12:48:02 -0400

    connected index html to server.js

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", async (req, res) => {
   console.log(req);
   const html = "<h1>Triangulate</h1>";
-  res.send("Triangulate");
+  res.render("index");
 });
 
 app.listen(PORT, () => console.log("App listening on port ", PORT));
