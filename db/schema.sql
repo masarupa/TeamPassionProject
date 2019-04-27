505a33a2dccb962131f8ac071d8aed1de12666e9
MeronLangsner <46253914+MeronLangsner@users.noreply.github.com>
2019-04-27 12:18:08 -0400

    database adjustments

--- db/schema.sql
+++ db/schema.sql
@@ -16,45 +16,19 @@ CREATE TABLE nycTransitHubs
 (13, 10)
 );
 
-INSERT INTO nycTransitHubs
-    (HubName, address, lat, long)
-VALUES
-    ("Times Square Station", "Q247+46 New York", 40.7553, 73.9869
-    );
-INSERT INTO nycTransitHubs
-    (HubName, address, lat, long)
-VALUES
-    ("Penn Station", "Q224+6H New York", 40.7506, 73.9935);
-INSERT INTO nycTransitHubs
-    (HubName, address, lat, long)
-VALUES
-    ("Grand Central Station", "Q23F+34 New York", 40.7527, 73.9772);
-INSERT INTO nycTransitHubs
-    (HubName, address, lat, long)
-VALUES
-    ("Columbus Circle", "Q299+97 New York", 40.7685, 73.9818);
-INSERT INTO nycTransitHubs
-    (HubName, address, lat, long)
-VALUES
-    ("Lexington & 59th", "Q27J+4X New York", 40.7628, 73.9676);
-INSERT INTO nycTransitHubs
-    (HubName, address, lat, long)
-VALUES
-    ("Lexington & 53rd", "Q25J+39 New York", 40.7571, 73.9719);
-INSERT INTO nycTransitHubs
-    (HubName, address, lat, long)
-VALUES
-    ("Union Square", "P2M5+WM New York", 40.7342,
-        73.9875);
-INSERT INTO nycTransitHubs
-    (HubName, address, lat, long)
-VALUES
-    ("Atlantic Ave/Barclay's Center", "M2MC+GF New York", 40.6837, 73.9788);
-INSERT INTO nycTransitHubs
-    (HubName, address, lat, long)
-VALUES
-    ("Queens Plaza/Queensboro Plaza", "P3X7+H3 New York", 40.7489, 73.9374);
-        /* Queens Plaza & Queensboro Plaza are distinct stations that are geographically on top of each other.  For our purposes they are of equal value as a meeting place.
+CREATE DATABASE Users_db;
 
+USE Users_db;
 
- 
+CREATE TABLE Users
+(
+    UserName VARCHAR
+(35) NOT NULL PRIMARY KEY,
+    email VARCHAR(45) NOT NULL,
+    address VARCHAR
+(100) NOT NULL,
+    lat DECIMAL
+(13, 10),
+    long DECIMAL
+(13, 10)
+);
