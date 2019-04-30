220b9fce7dcb7d467647ccfdef25d1cc56548daf
desham25 <runwiththebull84@gmail.com>
2019-04-30 19:29:35 -0400

    landing page

---
+++ public/javascript/initializefirebase.js
@@ -0,0 +1,10 @@
+// Initialize Fbase
+var config = {
+  apiKey: "AIzaSyCegsXRJeuV3qx32ag-9nVk47HPjo5Hvps",
+  authDomain: "teampassionproject.firebaseapp.com",
+  databaseURL: "https://teampassionproject.firebaseio.com",
+  projectId: "teampassionproject",
+  storageBucket: "teampassionproject.appspot.com",
+  messagingSenderId: "126210247332"
+};
+firebase.initializeApp(config);
