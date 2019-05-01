b817e3968d4eccc1b60f97361f6233250158947a
MeronLangsner <46253914+MeronLangsner@users.noreply.github.com>
2019-04-30 18:08:36 -0700

    added broad pseudocode steps as comments

--- maps.js
+++ maps.js
@@ -6,3 +6,13 @@ DirectionsService();
 route(request, callback);
 DirectionsRequest;
 function(DirectionsResult, DirectionsStatus);
+
+// ***Psuedocode of Triangulation Logic of V1***
+
+// User 1 and User 2 input locations in NYC
+
+// Brute force algorithim checks addresses against TransitHubs database
+
+// Compare the the results and find the two that are both lowest and closest together that lead to the same hub from the two locations
+
+// Return result of the Transit Hub
