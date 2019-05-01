bcdffaecc58a99f4113953ec445dcf41a94e48aa
MeronLangsner <46253914+MeronLangsner@users.noreply.github.com>
2019-04-30 17:56:02 -0700

    put up some of the info  from the Google Maps API page to be built into the app

---
+++ maps.js
@@ -0,0 +1,8 @@
+// maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap
+
+map = new google.maps.Map(document.getElementById("map"), {...});
+
+DirectionsService();
+route(request, callback);
+DirectionsRequest;
+function(DirectionsResult, DirectionsStatus);
